4742767b52814ac927c7beeea5a369ddc4b666e4
Andy Hansen <ahansen.nz@gmail.com>
2018-09-01 15:50:01 +1200

    Creating the stage for the rest of the project

---
+++ src/TypingRacer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './TypingRacer.scss';
+
+class TypingRacer extends Component {
+  render() {
+    return (
+      <div id="typing-racer">
+      </div>
+    );
+  }
+}
+
+export default TypingRacer;
