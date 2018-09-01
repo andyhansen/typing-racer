35d3fb1e100cedce2a964cf13560019e65f5be39
Andy Hansen <ahansen.nz@gmail.com>
2018-09-01 17:15:23 +1200

    Text input with tracking successful characters

---
+++ src/components/Input/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import './Input.scss'
+
+export default function({ onChange, value }) {
+  return (
+    <input className='challenge-input' onChange={onChange} value={value} />
+  )
+}
+
--- src/components/Stage/index.js
+++ src/components/Stage/index.js
@@ -3,20 +3,78 @@ import PropTypes from 'prop-types'
 
 import './Stage.scss'
 
+import Input from '../Input'
+
 class Stage extends Component {
   static propTypes = {
     typingChallenge: PropTypes.string.isRequired,
   }
 
   static defaultProps = {
-    typingChallenge: "It's a goopy bunch of text for you to start typing. How quickly can you do it!!",
+    typingChallenge: "It's a bunch of text for you to start typing. How quickly can you do it!!",
+  }
+
+  state = {
+    currentInput: '',
+    successfulCharacters: 0,
+    incorrectCharacters: 0,
+  }
+
+  updatedInput = (event) => {
+    const { typingChallenge } = this.props
+    const value = event.target.value
+    const minLength = Math.min(typingChallenge.length, value.length)
+
+    let successfulCharacters = 0
+    let incorrectCharacters = 0
+    let foundIncorrectCharacters = false
+    for (let i = 0; i < minLength; i++) {
+      if (
+        value.charAt(i) === typingChallenge.charAt(i)
+        && !foundIncorrectCharacters
+      ) {
+        successfulCharacters++
+      } else if (value.charAt(i) !== typingChallenge.charAt(i)) {
+        incorrectCharacters++
+        foundIncorrectCharacters = true
+      }
+    }
+
+    this.setState({ currentInput: value, successfulCharacters, incorrectCharacters })
+  }
+
+  getSuccessfulInput = () => {
+    const { typingChallenge } = this.props
+    const { successfulCharacters } = this.state
+    return typingChallenge.substr(0, successfulCharacters)
+  }
+
+  getIncorrectInput = () => {
+    const { typingChallenge } = this.props
+    const { successfulCharacters, incorrectCharacters } = this.state
+    return typingChallenge.substr(successfulCharacters, incorrectCharacters)
+  }
+
+  getRemainingInput = () => {
+    const { typingChallenge } = this.props
+    const { successfulCharacters, incorrectCharacters } = this.state
+    return typingChallenge.substr(successfulCharacters + incorrectCharacters)
   }
 
   render() {
+
     return (
       <div className='typing-stage'>
         <h2>Enter the text below as fast as you can.</h2>
-        <p className='challenge-text'>{ this.props.typingChallenge }</p>
+        <p className='challenge-text'>
+          <span className='successful-input'>{ this.getSuccessfulInput() }</span>
+          <span className='incorrect-input'>{ this.getIncorrectInput() }</span>
+          <span>{ this.getRemainingInput() }</span>
+        </p>
+        <Input
+          onChange={this.updatedInput}
+          value={this.state.currentInput}
+        />
       </div>
     )
   }
