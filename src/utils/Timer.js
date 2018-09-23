42f3a3ad8b68611f50fa18a6bb98473124600dbe
Andy Hansen <ahansen.nz@gmail.com>
2018-09-23 12:53:12 +1200

    Comments describing the methods

--- src/utils/Timer.js
+++ src/utils/Timer.js
@@ -15,26 +15,43 @@ class Timer {
     this.ticker = null
   }
 
+  /**
+   * Returns how many seconds the timer has been running.
+   */
   seconds () {
     return this.milliseconds / 1000
   }
 
+  /**
+   * Private method. Called at each interval to increment
+   * the tracked time, and invoke the callback, giving it
+   * the seconds elapsed so far.
+   */
   _tick () {
     this.milliseconds += this.interval
     if (this.callback) this.callback(this.seconds())
   }
 
+  /**
+   * Start the timer. Will not reset time elapsed.
+   */
   start () {
     if (this.ticker) return
     this.ticker = setInterval(this._tick, this.interval)
   }
 
+  /**
+   * Stop the timer.
+   */
   stop () {
     if (!this.ticker) return
     clearInterval(this.ticker)
     this.ticker = null
   }
 
+  /**
+   * Reset the timer.
+   */
   reset () {
     this.stop()
     this.ticker = null
